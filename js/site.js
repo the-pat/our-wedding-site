55eedc2b8c246648650f2f0989d2e7910b8172df
Patrick Tone <tone@thepat.xyz>
2017-06-04 10:09:08 -0500

    Added international addresses to RSVP section

--- js/site.js
+++ js/site.js
@@ -1,13 +1,5 @@
-$('a[href*=\\#]').on('click', function(event){
-  var el = $(this.hash);
-  if (el.length > 0) {
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-  }
-});
-
-$(document).ready(function() {
-  var i = 0;
+var addGuest = function() {
+  var i = 1;
   $('#addGuest').click(function() {
     var $row = $('#guest' + i);
     var $col = $('<div>', { class: 'col-lg-8 col-md-12' });
@@ -32,4 +24,39 @@ $(document).ready(function() {
     var $nextRow = $('<div>', { id: 'guest' + i });
     $row.after($nextRow);
   });
+};
+
+var bindCountries = function(data) {
+  var $ddl = $('#ddlCountry');
+
+  $.each(data, function() {
+    var $option = $('<option>', {
+      value: this,
+      html: this,
+    });
+
+    $ddl.append($option);
+  });
+};
+
+$(document).ready(function() {
+  addGuest();
+
+  // Smooth scrolling
+  $('a[href*=\\#]').on('click', function(event){
+    var el = $(this.hash);
+    if (el.length > 0) {
+      event.preventDefault();
+      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    }
+  });
+
+  // Get countries
+  $.ajax({
+    url: 'http://country.io/names.json',
+    dataType: 'jsonp',
+    success: bindCountries,
+  });
+
+  //$.getJSON('http://country.io/names.json', bindCountries);
 });
