d3e13986b5f4457ca49d320ddc5cccfbc234f545
Patrick Tone <tone@thepat.xyz>
2017-06-13 21:33:40 -0500

    Finished RSVP modal

--- js/contact.js
+++ js/contact.js
@@ -30,12 +30,12 @@ $(function() {
                 cache: false,
                 success: function() {
                     // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    $('#contact-success').html("<div class='alert alert-success'>");
+                    $('#contact-success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-success')
+                    $('#contact-success > .alert-success')
                         .append("<strong>Your message has been sent.</strong>");
-                    $('#success > .alert-success')
+                    $('#contact-success > .alert-success')
                         .append('</div>');
 
                     //clear all fields
@@ -43,11 +43,11 @@ $(function() {
                 },
                 error: function() {
                     // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    $('#contact-success').html("<div class='alert alert-danger'>");
+                    $('#contact-success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
+                    $('#contact-success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#contact-success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contact-form').trigger("reset");
                 },
--- js/site.js
+++ js/site.js
@@ -1,39 +1,106 @@
-var addGuest = function() {
-  var i = 1;
-  $('#addGuest').click(function() {
-    var $row = $('#guest' + i);
-    var $col = $('<div>', { class: 'col-lg-8 col-md-12' });
-    var $label = $('<label>', {
-      for: 'txtGuest' + i,
-      class: 'col-form-label',
-      text: 'Guest ' + i,
-    });
-    var $input = $('<input>', {
-      id: 'txtGuest' + i,
-      type: 'text',
-      name: 'Guest' + i,
-      class: "form-control border-olive",
-    });
-    $row.addClass('form-group row');
+$(function() {
+  var addGuest = function() {
+    var i = 1;
+    $('#rsvp-add-guest').click(function() {
+      var $row = $('#guest-' + i);
+      var $col = $('<div>', { class: 'col-lg-12' });
+      var $label = $('<label>', {
+        for: 'rsvp-guest-' + i,
+        class: 'col-form-label',
+        text: 'Guest ' + i,
+      });
+      var $input = $('<input>', {
+        id: 'rsvp-guest-' + i,
+        type: 'text',
+        name: 'Guest ' + i,
+        class: 'form-control',
+        placeholder: 'Your guest name'
+      });
+      var $p = $('<p>', { class: 'help-block text-danger' });
 
-    $col.append($label).append($input);
-    $row.append($col);
+      $row.addClass('row');
+      $col.append($label).append($input).append($p);
+      $row.append($col);
 
-    i++;
-    var $nextRow = $('<div>', { id: 'guest' + i });
-    $row.after($nextRow);
+      i++;
+      var $nextRow = $('<div>', { id: 'guest-' + i });
+      $row.after($nextRow);
+    });
+  };
+
+  $(document).ready(function() {
+    addGuest();
   });
-};
 
-$(document).ready(function() {
-  addGuest();
-
-  // Smooth scrolling
-  $('a[href*=\\#]').on('click', function(event){
-    var el = $(this.hash);
-    if (el.length > 0) {
+  $('form#rsvp-form input,textarea').jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event) {},
+    submitSuccess: function($form, event) {
       event.preventDefault();
-      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-    }
+
+      var name = $("input#rsvp-name").val();
+      var email = $("input#rsvp-email").val();
+      var address1 = $("input#rsvp-address-1").val();
+      var address2 = $("input#rsvp-address-2").val();
+      var city = $("input#rsvp-city").val();
+      var state = $("input#rsvp-state").val();
+      var zip = $("input#rsvp-zip").val();
+      var country = $('select#rsvp-country').val();
+      var gotcha = $("input#rsvp-gotcha").val();
+      var firstName = name;
+
+      // Check for white space in name for Success/Fail message
+      if (firstName.indexOf(' ') >= 0) {
+          firstName = name.split(' ').slice(0, -1).join(' ');
+      }
+
+      var data = {
+        name,
+        email,
+        address1,
+        address2,
+        city,
+        state,
+        zip,
+        country,
+        gotcha,
+      };
+
+      var $guests = $('input[id^="rsvp-guest-"]');
+      $.each($guests, function(key, value) {
+        data[value.name] = value.value;
+      });
+
+      $.ajax({
+        url: 'https://formspree.io/bojana.and.patrick@gmail.com',
+        type: 'POST',
+        data,
+        dataType: 'json',
+        cache: false,
+        success: function() {
+            // Success message
+            $('#rsvp-success').html("<div class='alert alert-success'>");
+            $('#rsvp-success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>");
+            $('#rsvp-success > .alert-success')
+                .append("<strong>Your message has been sent.</strong>");
+            $('#rsvp-success > .alert-success')
+                .append('</div>');
+
+            //clear all fields
+            $('#rsvp-form').trigger("reset");
+        },
+        error: function() {
+            // Fail message
+            $('#rsvp-success').html("<div class='alert alert-danger'>");
+            $('#rsvp-success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append("</button>");
+            $('#rsvp-success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+            $('#rsvp-success > .alert-danger').append('</div>');
+            //clear all fields
+            $('#rsvp-form').trigger("reset");
+        },
+      })
+    },
   });
 });
